59824c446c7331e86615c17b8f0713712a633e2e
SuryaMasabathula <surya.pendimi@gmail.com>
2016-09-19 15:47:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,6 +13,14 @@ app.get('/article_one', function (req, res) {
   res.send('Article one was requested and served here!');
 });
 
+app.get('/article_one', function (req, res) {
+  res.send('Article two was requested and served here!');
+});
+
+app.get('/article_one', function (req, res) {
+  res.send('Article three was requested and served here!');
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
