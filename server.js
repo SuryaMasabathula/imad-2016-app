3b100382330e5be424b63e8ca07d0a4d74367e2f
SuryaMasabathula <surya.pendimi@gmail.com>
2016-09-21 15:24:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,33 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleOne={
+ title:"Article One",
+ heading: "Primary Education",
+ date:"Sept 21st 2016",
+ content:`The Indian government lays emphasis on primary education, also referred to as elementary education, to children aged 6 to 14 years old.The Indian government has also banned child labour in order to ensure that the children do not enter unsafe working conditions.However, both free education and the ban on child labour are difficult to enforce due to economic disparity and social conditions.80% of all recognised schools at the elementary stage are government run or supported, making it the largest provider of education in the country.`
+};
+
+var htmlObject={
+    <html>
+  <head>
+        <title>${title}</title>
+  </head>
+  <body>
+      <div class="container">
+      <h1> ${title} </h1>  
+      <h2>Here it starts... ${title} </h2>
+      <h3>${heading}</h3>
+      <p>
+       ${content}
+      </p>
+     </div>
+  </body>
+</html>
+    
+};
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
