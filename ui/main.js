3989ea6681e269dfcc84b18339774a79de15b89b
SuryaMasabathula <surya.pendimi@gmail.com>
2016-09-22 17:26:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,8 @@ console.log('Loaded!');
 //change the text of the div id main11
 var element=document.getElementById('main11');
 element.innerHTML='Hello Heloo Hello............................................';
+
+var element=document.getElementById('madi');
+img.onclick=function(){
+    img.style.marginright='200px';
+};
