4d7aeb16349a3f66f84b0e7f9d51a15e7cebc74e
SuryaMasabathula <surya.pendimi@gmail.com>
2016-09-22 16:49:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ function createTemplate(data)
 }
 
 app.get('/', function (req, res) {
-  res.send(path.join(__dirname, 'ui', 'index.html'));
+  res.sendfile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/:articleName', function (req, res) {
