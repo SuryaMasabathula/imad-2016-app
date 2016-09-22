0908224da05871718cf49a99dce39a44bf13c732
SuryaMasabathula <surya.pendimi@gmail.com>
2016-09-22 17:20:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,4 @@
 console.log('Loaded!');
 //change the text of the div id main11
 var element=document.getElementById('main11');
-element.innerHtml="Hello Heloo Hello............................................";
+element.innerHTML='Hello Heloo Hello............................................';
