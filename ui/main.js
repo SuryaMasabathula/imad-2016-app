d1e649bd563000ca35ed62b503a3ea96d4e344ae
SuryaMasabathula <surya.pendimi@gmail.com>
2016-09-23 14:59:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,6 +4,14 @@ var element=document.getElementById('main11');
 element.innerHTML='Hello Heloo Hello............................................';
 
 var img=document.getElementById('madi');
+
+
+var marginLeft=0;
+function marginRight(){
+    marginLeft=marginLeft + 10;
+     img.style.marginRight= marginLeft + 'px';
+}
+
 img.onclick=function(){
-    img.style.marginLeft='200px';
+    var interval=setInterval(marginRight,100);
 };
